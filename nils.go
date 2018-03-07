2edf2ded00ccaa9566f89836068153011c9d8f59
Ryan Bastic <ryan@gooddoglabs.com>
2018-03-07 23:42:38 +0100

    add strings in here for now

--- nils.go
+++ nils.go
@@ -29,3 +29,33 @@ func RemoveNilsIfNeeded(maybeNils []interface{}) []interface{} {
 	}
 	return maybeNils
 }
+
+func CountEmptyStrings(maybeEmpty []string) int {
+	var count int
+	for _, v := range maybeEmpty {
+		if v == "" {
+			count++
+		}
+	}
+	return count
+}
+
+func RemoveEmptyStrings(someEmpty []string, count int) []string {
+	noEmpty := make([]interface{}, len(someEmpty)-count)
+	j := 0
+	for _, v := range someEmpty {
+		if v != "" {
+			noEmpty[j] = v
+			j++
+		}
+	}
+	return noEmpty
+}
+
+func RemoveEmptyStringsIfNeeded(maybeEmpty []string) []string {
+	numEmpty := CountEmptyStrings(maybeEmpty)
+	if numEmpty > 0 {
+		return RemoveEmptyStrings(maybeEmpty, numEmpty)
+	}
+	return maybeEmpty
+}
